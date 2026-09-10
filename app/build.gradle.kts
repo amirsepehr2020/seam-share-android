f7446720ce465d74a2c02fdeb45eb5e3edd7a981
SEPEHR <sepehr2sodoury@gmail.com>
2026-09-10 23:08:45 +0330

    feat: add Android destination folder support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
