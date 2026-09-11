568db0ef254dd7d20192228e1b853abc49c02160
SEPEHR <sepehr2sodoury@gmail.com>
2026-09-11 12:48:46 +0330

    test: cover E2E framing boundary cases

---
+++ app/src/test/java/ir/redlighte/seamshare/network/SeamE2eProtocolTest.kt
@@ -0,0 +1,25 @@
+package ir.redlighte.seamshare.network
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SeamE2eProtocolTest {
+    @Test
+    fun ciphertextSizeHandlesZeroAndChunkBoundaries() {
+        assertEquals(16L, SeamE2eProtocol.ciphertextSize(0))
+        assertEquals(17L, SeamE2eProtocol.ciphertextSize(1))
+        assertEquals(256L * 1024L + 16L, SeamE2eProtocol.ciphertextSize(256L * 1024L))
+        assertEquals(256L * 1024L + 32L, SeamE2eProtocol.ciphertextSize(256L * 1024L + 1L))
+    }
+
+    @Test
+    fun nonceIsUniquePerChunk() {
+        val prefix = byteArrayOf(0x10, 0x20, 0x30, 0x40)
+        assertEquals("102030400000000000000000", prefixNonce(prefix, 0))
+        assertEquals("102030400000000000000001", prefixNonce(prefix, 1))
+        assertEquals("102030400000000000000002", prefixNonce(prefix, 2))
+    }
+
+    private fun prefixNonce(prefix: ByteArray, index: Long): String =
+        SeamE2eProtocol.nonce(prefix, index).joinToString("") { "%02x".format(it) }
+}
